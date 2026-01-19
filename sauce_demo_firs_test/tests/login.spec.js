57ed4c5b8f4558e921b4e1fd89616698ec945a34
Hannah Gavrilova <anya94qwe@gmail.com>
2026-01-19 19:07:45 +0300

    refactor: change functions

--- sauce_demo_firs_test/tests/login.spec.js
+++ sauce_demo_firs_test/tests/login.spec.js
@@ -1,19 +1,38 @@
+// Импортируем 'test' и 'expect' из библиотеки Playwright
 import { test, expect } from '@playwright/test';
 
-test('User automation ', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  //await expect(page).toHaveTitle(/Swag Labs/); //проверка соответствия title страницы
+// Описываем наш набор тестов
+test.describe('Авторизация на Sauce Demo', () => {
+  // Создаем тест-кейс
+  test('Пользователь должен успешно войти в систему', async ({ page }) => {
+    // 1. Переходим на страницу
+    await page.goto('https://www.saucedemo.com/');
+    //await expect(page).toHaveTitle(/Swag Labs/); //проверка соответствия title страницы
 
-  //const userName = page.locator("[data-test='user-name']");
-  const userName = page.locator('#user-name');
+    // 2. Вводим логин
+    //const userName = page.locator("[data-test='user-name']"); //загрузка более 5 сек. поэтому поломал тест
+    // Используем селектор по id + каждый шаг
+/*  const userName = page.locator('#user-name');
   await expect(userName).toBeVisible(); //ожидание готовности/загрузки эл-та
   await userName.fill('standard_user'); // ввод данных
-
+*/
+    //все в 1й сроке
+    await page.locator('#user-name').fill('standard_user');
+/*
   const userPassword = page.locator("[data-test='password']");
   await expect(userPassword).toBeVisible(); //ожидание готовности/загрузки эл-та
   await userPassword.fill('secret_sauce'); // ввод данных
+*/
+    await page.locator('[placeholder="Password"]').fill('secret_sauce');
 
+    /*
   const userLoginBtn = page.locator("[data-test='login-button']");
   await expect(userLoginBtn).toBeEnabled(); //ожидание готовности/загрузки эл-та
   await userLoginBtn.click();
+*/
+    await page.locator('[data-test="login-button"]').click();
+
+    // 5. Проверяем, что URL изменился и содержит нужную часть
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+  });
 });
